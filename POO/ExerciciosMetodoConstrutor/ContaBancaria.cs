ae690c01c89f3b7eb9e2e4cf133e0e9870d839b2
Walyson Menezes <menezeswalyson@gmail.com>
2025-11-06 13:04:59 -0300

    Subindo atividade em grupo encapsulamento em C#

--- POO/ExerciciosMetodoConstrutor/ContaBancaria.cs
+++ POO/ExerciciosMetodoConstrutor/ContaBancaria.cs
@@ -15,10 +15,35 @@ namespace ExerciciosMetodoConstrutor
             titular = T;
             saldo = S;
         }
-        
+
         public void Dadosconta()
         {
             System.Console.WriteLine($"A conta to titular {titular}, tem o saldo de R${saldo}");
         }
     }
 }
+
+public class ContaBancaria
+{
+    private double saldo; // dado protegido
+    public double Saldo //propriedade
+    {
+        get { return saldo; }          // Permite ler o valor
+        private set                   // Só pode ser alterado dentro da classe
+        {
+            if (value >= 0)            // Garante que o saldo nunca seja negativo
+                saldo = value;
+        }
+    }
+    public void Depositar(double valor)
+    {
+        if (valor > 0)
+            saldo += valor;
+    }
+
+    public double ConsultarSaldo()
+    {
+        return saldo;
+    }
+}
+    
